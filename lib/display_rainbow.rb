d95b91c01dd19d49828a7deee152c6034c81b6a6
Carden Z <ephemere@protonmail.com>
2018-11-20 02:38:15 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  puts "The first color in the rainbos is: #{letters[1]}"
+	puts "The second color in the rainbos is: #{letters[2]}"
+	puts "The third color in the rainbos is: #{letters[3]}"
+	puts "The fourth color in the rainbos is: #{letters[4]}"
+	puts "The fifth color in the rainbos is: #{letters[5]}"
+	puts "The sixth color in the rainbos is: #{letters[6]}"
+	puts "The last color in the rainbos is: #{letters[7]}"
+end
